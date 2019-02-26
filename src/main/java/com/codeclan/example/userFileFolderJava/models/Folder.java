7c032c75d6722a860e9ecf02ec2c3744b4059b2b
Joe McColl <joe4kool@gmail.com>
2019-02-26 18:18:20 +0000

    added constructor, getters and setters for Folder class

--- src/main/java/com/codeclan/example/userFileFolderJava/models/Folder.java
+++ src/main/java/com/codeclan/example/userFileFolderJava/models/Folder.java
@@ -1,4 +1,49 @@
 package com.codeclan.example.userFileFolderJava.models;
 
+import java.util.List;
+
 public class Folder {
+
+	private long id;
+	private String title;
+	private List<File> files;
+	private User user;
+
+	public Folder(String title, List<File> files, User user) {
+		this.title = title;
+		this.files = files;
+		this.user = user;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<File> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<File> files) {
+		this.files = files;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
