e000edd6aef580fb7eb7a0c3b4a01d64add9811f
Joe McColl <joe4kool@gmail.com>
2019-02-26 18:03:13 +0000

    initial git commit with basic file structure

---
+++ src/main/java/com/codeclan/example/userFileFolderJava/UserFileFolderJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.userFileFolderJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFileFolderJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFileFolderJavaApplication.class, args);
+	}
+
+}
